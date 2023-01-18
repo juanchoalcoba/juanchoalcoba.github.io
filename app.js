9d3cf0d302809f56ed974756aaaad288cf7fef63
Juan Alcoba <107285285+juanchoalcoba@users.noreply.github.com>
2023-01-18 09:55:24 -0300

    Edit Commit

---
+++ app.js
@@ -0,0 +1,56 @@
+//FUNCION QUE ME APLICA EL ESTILO A LA OPCION SELECCIONADA EN EL MENU Y QUITA LA PREVIAMENTE SELECCIONADA
+function seleccionar(link){
+    var opciones = document.querySelectorAll("#links a");
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";    
+    link.className = "seleccionado";
+
+    //hacemos desaparecer el menu una vez que se ha seleccionado una opcion en el modo responsive
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+
+
+
+
+//FUNCION QUE MUESTRA EL MENU RESPONSIVE
+function responsiveMenu(){
+    var x = document.getElementById("nav");
+    if(x.className === ""){
+        x.className = "responsive";
+    }else{
+        x.className = "";
+    }
+}
+
+
+//DETECTO EL SCROLLING PARA ANIMAR
+window.onscroll = function(){
+    efectoHabilidades()
+};
+
+//funcion que aplica la animacion de la barra de habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if (distancia_skills >= 300){
+        document.getElementById("html").classList.add("barra-progreso1");
+        document.getElementById("js").classList.add("barra-progreso2");
+        document.getElementById("bd").classList.add("barra-progreso3");
+        document.getElementById("ps").classList.add("barra-progreso4");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
