3cf4042ce28d98ac50cc8eac5c82538f54ac1f4b
Juan Alcoba <juanchoalcoba@gmail.com>
2023-08-11 11:23:51 -0300

    update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-  window.onload = function() {
+
+window.onload = function() {
             var loading = document.getElementById('loading');
             var content = document.getElementById('content');
 
@@ -11,7 +12,7 @@
                 setTimeout(function() {
                     content.style.opacity = '1';
                 }, 100);
-            }, 1500);
+            }, 1200);
         };
 
 //FUNCION QUE ME APLICA EL ESTILO A LA OPCION SELECCIONADA EN EL MENU Y QUITA LA PREVIAMENTE SELECCIONADA
@@ -59,6 +60,37 @@ function efectoHabilidades(){
         document.getElementById("ps").classList.add("barra-progreso4");
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const modal = document.getElementById("cookie-modal");
+    const overlay = document.getElementById("modal-overlay");
+  
+    function openModal() {
+      modal.style.display = "block";
+      overlay.style.display = "block";
+    }
+  
+    function closeModal() {
+      modal.style.display = "none";
+      overlay.style.display = "none";
+    }
+  
+    const acceptButton = document.getElementById("accept-cookies");
+    const rejectButton = document.getElementById("reject-cookies");
+  
+    acceptButton.addEventListener("click", closeModal);
+    rejectButton.addEventListener("click", closeModal);
+  
+    // Mostrar el modal automáticamente cuando la página se carga
+    openModal();
+  });
+
+
+
+
+
+
+
 
 
 
